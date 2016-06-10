daf6935448772d9922d123bfd5f3852f66f7d968
dennisfelippe <zero_dfvn@hotmail.com>
2016-06-10 19:55:27 -0300

    Correção

--- Exercicios 02/Resposta 05.rb
+++ Exercicios 02/Resposta 05.rb
@@ -7,6 +7,7 @@
 require 'time'
 t = Time.new( 1986, 11)
 ano = 60*60*24*365.25
+puts (t - ano*2*75.3).strftime('%m/%Y')
 puts (t - ano*75.3).strftime('%m/%Y') #Aparicao Anterior
 t2 =Time.new( 3000)
 while t < t2 do
